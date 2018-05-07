f65e57aa2c65ee2e58a41c8c1ce97c12b228c31c
Tao Du <dutao09@gmail.com>
2018-05-06 20:00:45 -0400

    Fix build error.

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -8,6 +8,8 @@ rm Eigen.3.3.4.zip
 
 # Download and unzip GLEW because building from the source is a huge pain, as
 # suggested in their website.
+# Tls12 is needed to successfully download glew.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 wget https://github.com/nigels-com/glew/releases/download/glew-2.0.0/glew-2.0.0.zip -OutFile glew.2.0.0.zip
 Expand-Archive glew.2.0.0.zip -DestinationPath externals
 # Rename the folder.
