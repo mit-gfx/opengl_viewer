ebda9ead95ce8d5ef064643cee1c5581ae36cfc4
Tao Du <taodu@csail.mit.edu>
2019-06-25 15:19:02 -0400

    Finished the cornell box example.

--- python/example_cornell_box.py
+++ python/example_cornell_box.py
@@ -29,6 +29,7 @@ vertex = np.array([[-1, 0, -1],
                    [1, 0, 1]])
 face = np.array([[0, 2, 3],
                  [0, 3, 1]])
+gray = np.array([0.85, 0.85, 0.85])
 viewer.add_static_object(vertex, face, object_options)
 
 object_options["model matrix"] = [[1, 0, 0, 0],
@@ -63,8 +64,35 @@ object_options["ambient"] = [0, 0.5, 0.5]
 object_options["diffuse"] = [0, 0.5, 0.5]
 viewer.add_static_object(vertex, face, object_options)
 
+
+object_options["model matrix"] = [[0.25, 0, 0, 0],
+                                  [0, 0.25, 0, -0.5],
+                                  [0, 0, 0.25, 0],
+                                  [0, 0, 0, 1]]
+object_options["ambient"] = gray
+viewer.add_static_object(vertex, face, object_options)
+object_options["model matrix"] = [[0.25, 0, 0, 0],
+                                  [0, -0.25, 0, 0.5],
+                                  [0, 0, -0.25, 0],
+                                  [0, 0, 0, 1]]
+viewer.add_static_object(vertex, face, object_options)
+object_options["model matrix"] = [[0.25, 0, 0, 0],
+                                  [0, 0, -0.25, 0],
+                                  [0, 0.25, 0, -0.5],
+                                  [0, 0, 0, 1]]
+viewer.add_static_object(vertex, face, object_options)
+object_options["model matrix"] = [[0, 0.25, 0, -0.5],
+                                  [-0.25, 0, 0, 0],
+                                  [0, 0, 0.25, 0],
+                                  [0, 0, 0, 1]]
+viewer.add_static_object(vertex, face, object_options)
+object_options["model matrix"] = [[0, -0.25, 0, 0.5],
+                                  [0.25, 0, 0, 0],
+                                  [0, 0, 0.25, 0],
+                                  [0, 0, 0, 1]]
+viewer.add_static_object(vertex, face, object_options)
+
 light_option = PyglOption()
-gray = np.array([0.85, 0.85, 0.85])
 light_option["ambient"] = gray * 0.5
 light_option["diffuse"] = gray * 0.5
 light_option["specular"] = gray * 0.5
